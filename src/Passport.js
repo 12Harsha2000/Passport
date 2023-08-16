f9a6c561ae8b6cf3d113634b6726dcab290b1de9
Harsha Maria Jose <harshamariajose.20@gmail.com>
2023-08-16 10:53:04 +0530

    done

---
+++ src/Passport.js
@@ -0,0 +1,141 @@
+import React, { useState } from 'react';
+
+function Passport() {
+  const initialFormData = {
+    firstName: '',
+    Nationality: '',
+    phone: '',
+    gender: '',
+    checkbox: '',
+
+  };
+
+  const [formData, setFormData] = useState(initialFormData);
+
+
+  const handleChange = (event) => {
+    const { name, value, type, checked } = event.target;
+    const inputValue = type === 'radio' || type === 'checkbox' ? checked : value;
+
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: inputValue,
+    }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('Form data submitted:', formData);
+    setFormData(initialFormData);
+  };
+  return (
+    <div>
+      <h1>Paaport Application Form</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          First Name:
+          <input
+            type="text"
+            name="firstName"
+            value={formData.firstName}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+
+
+        <label>
+          Nationality:
+          <input
+            type="text"
+            name="Nationality"
+            value={formData.Nationality}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+
+        <label>
+          Email:
+          <input
+            type="email"
+            name="email1"
+            value={formData.email}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+
+
+        <label>
+          Mobile number:
+          <input
+            type="tel"
+            name="phone"
+            value={formData.phone}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+
+        <label>
+          Gender :
+          <input
+            type="radio"
+            name="gender"
+            value="male"
+            onChange={handleChange}
+          />
+          Male
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="gender"
+            value="female"
+            onChange={handleChange}
+          />
+          Female
+        </label>
+        <br />
+
+        <label>
+          Address:
+          <textarea
+            name="address"
+            value={formData.address}
+            onchange={handleChange}
+          />
+        </label>
+        <br />
+
+        <label>
+          CGPA:
+          <input
+            type="tel"
+            name="number"
+            value="cgpa"
+            onchange={handleChange}
+          />
+        </label>
+        <br />
+
+        <label>
+          <input
+            type="checkbox"
+            name="checkbox"
+            checked={formData.checkbox}
+            onChange={handleChange}
+            required
+          />
+          I have read and agree to all terms and conditions.
+        </label>
+        <br />
+
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default Passport;

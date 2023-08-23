29e6409e6e5cdb974bf89ee951947b0c8abdfed7
Harsha Maria Jose <harshamariajose.20@gmail.com>
2023-08-23 15:15:23 +0530

    change made

--- src/components/List.js
+++ src/components/List.js
@@ -1,16 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const List = () => {
+  const [centers, setCenters] = useState([
+    {
+      name: 'Kochi Passport Center',
+      address: '123 Main Street, Kochi',
+      timings: '9:00 AM - 5:00 PM',
+    },
+    {
+      name: 'Kottayam Passport Center',
+      address: '456 Park Avenue, Kottayam',
+      timings: '8:30 AM - 4:30 PM',
+    },
+    {
+      name: 'Kannur Passport Center',
+      address: '789 Elm Road, Kannur',
+      timings: '9:30 AM - 6:00 PM',
+    },
+    {
+      name: 'Kozhikkode Passport Center',
+      address: '567 Oak Lane, Kozhikkode',
+      timings: '10:00 AM - 7:00 PM',
+    },
+  ]);
+
   return (
     <div>
-      <h1> Passport Centers List </h1>
-   <ul>
-     <li> kochi </li>
-     <li> kottayam </li>
-     <li> kannur </li>
-     <li> kozhikkode </li>
-   </ul>
-   </div>
- )  
+      <h1>Passport Centers List</h1>
+      <ul>
+        {centers.map((center, index) => (
+          <li key={index}>
+            <h2>{center.name}</h2>
+            <p>{center.address}</p>
+            <p>Timings: {center.timings}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
-export default List
+
+export default List;
